2f3670b486d4f0ef16e9fa093f96730d0d974176
Manoj <mailmanoj123@gmail.com>
2017-10-25 10:27:24 +0530

    init

---
+++ krishu/src/main/java/krishu/KrishuApplication.java
@@ -0,0 +1,12 @@
+package krishu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrishuApplication.class, args);
+	}
+}
